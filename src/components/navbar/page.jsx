24d1537cbe986645afccfa8e9824db89ee87238d
Muhammad Ishaq <iosoftnet@gmail.com>
2025-05-17 01:44:58 +0400

    update expeirence

--- src/components/navbar/page.jsx
+++ src/components/navbar/page.jsx
@@ -14,7 +14,11 @@ const AppBar = () => {
       <Navbar expand="sm" className="w-100">
         <Container>
           <Navbar.Brand className={styles.name} href="/">
+<<<<<<< HEAD
             Muhammad Ishaq
+=======
+           M ishaq
+>>>>>>> ab32915 (navabr change)
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="offcanvasNavbar-expand-sm" className={styles.toggle}/>
           <Navbar.Offcanvas
